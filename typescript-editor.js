b99db15a2300b9e66c6d26b26a27e1e621b05c4a
Cody Ebberson <cody@ebberson.com>
2023-09-14 09:25:01 -0700

    Handle empty messages

--- typescript-editor.js
+++ typescript-editor.js
@@ -11,8 +11,11 @@ if (module === "commonjs") {
 
 const target = module === "esnext" ? "esnext" : "es6";
 
-let editor = undefined;
-let tsProxy = undefined;
+const state = {
+  editor: undefined,
+  tsProxy: undefined,
+  initialCode: "",
+};
 
 console.log("module", module);
 console.log("target", target);
@@ -49,30 +52,49 @@ require([
     main,
     window.ts
   );
-  editor = sandbox.editor;
-  editor.getModel().updateOptions({ tabSize: 2 });
-  editor.focus();
+  state.editor = sandbox.editor;
+  state.editor.getModel().updateOptions({ tabSize: 2 });
+  state.editor.focus();
 
   // Get transpiled output
   // See: https://stackoverflow.com/a/55132486
   monaco.languages.typescript.getTypeScriptWorker().then((worker) => {
-    worker(editor.getModel().uri).then((proxy) => (tsProxy = proxy));
+    worker(state.editor.getModel().uri).then((proxy) => {
+      state.tsProxy = proxy;
+      if (state.initialCode) {
+        state.editor.getModel().setValue(state.initialCode);
+      }
+    });
   });
 });
 
 window.addEventListener("message", (e) => {
-  if (e.data?.command === "setValue" && editor) {
-    editor.getModel().setValue(e.data.value);
+  if (e.data?.command === "setValue") {
+    if (state.editor && state.tsProxy) {
+      state.editor.getModel().setValue(e.data.value ?? "");
+    } else {
+      state.initialCode = e.data.value ?? "";
+    }
     e.ports[0].postMessage({ result: true });
   }
 
-  if (e.data?.command === "getValue" && editor) {
-    e.ports[0].postMessage({ result: editor.getValue() });
+  if (e.data?.command === "getValue") {
+    let result = state.initialCode;
+    if (state.editor && state.tsProxy) {
+      result = state.editor.getValue();
+    }
+    e.ports[0].postMessage({ result });
   }
 
-  if (e.data?.command === "getOutput" && editor && tsProxy) {
-    tsProxy.getEmitOutput(editor.getModel().uri.toString()).then((r) => {
-      e.ports[0].postMessage({ result: r.outputFiles[0].text });
-    });
+  if (e.data?.command === "getOutput") {
+    if (state.editor && state.tsProxy) {
+      state.tsProxy
+        .getEmitOutput(state.editor.getModel().uri.toString())
+        .then((r) => {
+          e.ports[0].postMessage({ result: r.outputFiles[0].text });
+        });
+    } else {
+      e.ports[0].postMessage({ result: "" });
+    }
   }
 });
